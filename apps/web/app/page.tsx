eca6787396a4653ad8eac5bf411e462a292dbef0
Gene Lin <genelin0128@gmail.com>
2025-09-29 23:45:28 -0500

    chore: tweak homepage copy

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ export default function HomePage() {
     <main className="mx-auto max-w-2xl p-6">
       <h1 className="text-3xl font-bold">Gene Software</h1>
       <p className="mt-4 text-gray-600">
-        Hello! This is the home page. We&apos;ll add About/Links soon.
+        Hello! This is the home page. Gene will add About/Links soon.
       </p>
     </main>
   );
